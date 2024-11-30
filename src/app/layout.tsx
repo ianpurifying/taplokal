57f4471a24a10cb3293c69fe1764aeea3ffbad5f
IANNY002 <sacredmind2002@gmail.com>
2024-11-30 16:50:15 +0800

    add packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./styles/globals.css";
+import "./globals.css";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import { Suspense } from "react";
