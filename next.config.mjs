92f6e30129c99b8cb69e1e9b4bd3c61cefc3d50b
IANNY002 <sacredmind2002@gmail.com>
2024-12-02 15:55:29 +0800

    fix page error

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-            "firebasestorage.googleapis.com"
-        ],
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
